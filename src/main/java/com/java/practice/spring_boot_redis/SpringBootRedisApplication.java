528228ed71809f8eb73bc93895cd7d72dc8458d2
sagarm_gitlab <sagarmahangare90@gmail.com>
2024-07-28 15:27:07 +0530

    first commit

---
+++ src/main/java/com/java/practice/spring_boot_redis/SpringBootRedisApplication.java
@@ -0,0 +1,13 @@
+package com.java.practice.spring_boot_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRedisApplication.class, args);
+	}
+
+}
